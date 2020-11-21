431661fe7f2f39830879884c685c0058b48fec16
michelangelo truffarelli <michelangelo.truffarelli@outlook.com>
2020-11-21 19:11:27 +0100

    Add project files.

---
+++ Punto3D/Program.cs
@@ -0,0 +1,14 @@
+﻿using Punto2D;
+using System;
+
+namespace Punto3D
+{
+    public class Punti3D : Punti2D
+    {
+        public int z { get; private set; }
+        public Punti3D(int x, int y, int z) : base(x, y)
+        {
+            this.z = z;
+        }
+    }
+}
